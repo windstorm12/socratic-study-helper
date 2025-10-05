61f3e957a77fb1fb0d359956e964574666e8d8d2
windstorm12 <115856461+windstorm12@users.noreply.github.com>
2025-10-05 08:26:51 -0700

    Fixed all merging issues

--- Google_gemini.py
+++ Google_gemini.py
@@ -77,6 +77,7 @@ def get_answer(user_subject, user_input):
     Do not give direct answers. Be curious, patient, and encouraging.
     You can only generate 1 question
     The user is talking about {user_subject} and they just said {user_input}, the conversation history is {conversation}
+    IF you think the user fully understands the topic, return to the user's first message in {conversation} and move on to the next part in the user's message
     Once you feel like the user has understood the topic well, return to the user's first message which is {first_message} and move on to the next topic that is presented in the message
     BUT only move on if you are 100% sure the user has understood the topic well. If you are not sure, keep asking more questions about the current topic.
     """
@@ -93,6 +94,10 @@ def get_answer(user_subject, user_input):
 USERS = {
     "Avnish": "Nerd",
     "Krish": "Newbie",
+    "Ashwanth": "Black Monkey",
+    "Swaroop": "Stupid",
+    "Anuj": "SmartGuy",
+    "LernerClass": "12345",
     "Ashwanth": "Chipat",
     "Swaroop": "Stupid",
     "Windstorm": "AlanMcBob",
